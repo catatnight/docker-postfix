34a8fae0f0e123d8064dc7b58eed8b056110ee72
Isaacs, Ben <ben.isaacs@cbsinteractive.com>
2017-11-30 07:21:04 +0000

    Better support for:
    * virtualdomains
    * TLS
    * Docker debugging
    * smtp auth only on submission port
    * letsencrypt

--- assets/install.sh
+++ assets/install.sh
@@ -29,8 +29,31 @@ chmod +x /opt/postfix.sh
 postconf -e myhostname=$maildomain
 postconf -F '*/*/chroot = n'
 
+# set up some email security as per https://ssl-tools.net/mailservers/ and 
+# https://serverfault.com/questions/670348/how-to-force-a-own-set-of-ciphers-in-postfix-2-11#670359 and
+# https://blog.tinned-software.net/harden-the-ssl-configuration-of-your-mailserver/
+
+postconf -e tls_ssl_options=NO_COMPRESSION
+
 postconf -e smtp_use_tls=yes
 postconf -e smtp_tls_security_level=may
+postconf -e "smtp_tls_protocols = !SSLv2, !SSLv3"
+postconf -e "smtp_tls_mandatory_protocols = !SSLv2, !SSLv3"
+postconf -e "smtp_tls_exclude_ciphers = DES-CBC3-SHA, EDH-RSA-DES-CBC3-SHA, RC2, RC4, aNULL"
+postconf -e smtp_tls_loglevel=1
+
+postconf -e smtpd_tls_security_level=may
+# postconf -e smtpd_tls_auth_only=yes
+postconf -e "smtpd_tls_protocols = !SSLv2, !SSLv3"
+postconf -e "smtpd_tls_mandatory_protocols = !SSLv2, !SSLv3"
+postconf -e smtpd_tls_ciphers=high
+postconf -e smtpd_tls_mandatory_ciphers=high
+postconf -e "smtpd_tls_exclude_ciphers = DES-CBC3-SHA, EDH-RSA-DES-CBC3-SHA, RC2, RC4, aNULL"
+postconf -e smtpd_tls_eecdh_grade=ultra
+
+# include 172.16/12 for docker
+postconf -e "mynetworks=127.0.0.0/8 [::ffff:127.0.0.0]/104 [::1]/128 172.16.0.0/12"
+
 
 ############
 # SASL SUPPORT FOR CLIENTS
@@ -38,9 +61,15 @@ postconf -e smtp_tls_security_level=may
 # Cyrus-SASL support for authentication of mail clients.
 ############
 # /etc/postfix/main.cf
-postconf -e smtpd_sasl_auth_enable=yes
+
+# this is enabled separately for smpt 25 and submission 587
+#postconf -e smtpd_sasl_auth_enable=yes
+
 postconf -e broken_sasl_auth_clients=yes
-postconf -e smtpd_recipient_restrictions=permit_sasl_authenticated,reject_unauth_destination
+postconf -e smtpd_sasl_security_options=noanonymous
+postconf -e "smtpd_recipient_restrictions=permit_mynetworks, permit_sasl_authenticated, reject_unauth_destination"
+postconf -e "smtpd_relay_restrictions = permit_mynetworks, permit_sasl_authenticated, reject_unauth_destination"
+
 # smtpd.conf
 cat >> /etc/postfix/sasl/smtpd.conf <<EOF
 pwcheck_method: auxprop
@@ -59,24 +88,42 @@ chown postfix.sasl /etc/sasldb2
 ############
 if [[ "$virtual_domains" != "" ]]; then
   postconf -e virtual_alias_maps=hash:/etc/postfix/virtual
-  postconf -e virtual_alias_domains=$virtual_domains
+  postconf -e "virtual_alias_domains=$virtual_domains"
+  postmap /etc/postfix/virtual
 fi
 
 ############
 # Enable TLS
 ############
+
 if [[ -n "$(find /etc/postfix/certs -iname *.crt)" && -n "$(find /etc/postfix/certs -iname *.key)" ]]; then
+  SMTPD_TLS_CERT_FILE=$(find /etc/postfix/certs -iname *.crt)
+  SMTPD_TLS_KEY_FILE=$(find /etc/postfix/certs -iname *.key)
+fi
+
+# for letsencrypt
+if [[ -n "$(find /etc/postfix/certs -iname *.pem)" ]]; then
+  SMTPD_TLS_CERT_FILE=$(find /etc/postfix/certs -iname *full*.pem)
+  SMTPD_TLS_KEY_FILE=$(find /etc/postfix/certs -iname *priv*.pem)
+fi
+
+if [[ "$SMTPD_TLS_CERT_FILE" != "" ]]; then
   # /etc/postfix/main.cf
-  postconf -e smtpd_tls_cert_file=$(find /etc/postfix/certs -iname *.crt)
-  postconf -e smtpd_tls_key_file=$(find /etc/postfix/certs -iname *.key)
+  postconf -e smtpd_tls_cert_file=$SMTPD_TLS_CERT_FILE
+  postconf -e smtpd_tls_key_file=$SMTPD_TLS_KEY_FILE
   chmod 400 /etc/postfix/certs/*.*
   # /etc/postfix/master.cf
+  #postconf -M submission/inet="submission   inet   n   -   n   -   -   smtpd -v" # enable for debugging auth on 587
   postconf -M submission/inet="submission   inet   n   -   n   -   -   smtpd"
   postconf -P "submission/inet/syslog_name=postfix/submission"
-  postconf -P "submission/inet/smtpd_tls_security_level=encrypt"
+  postconf -P "submission/inet/smtpd_tls_security_level=encrypt" # comment out to test AUTH
   postconf -P "submission/inet/smtpd_sasl_auth_enable=yes"
   postconf -P "submission/inet/milter_macro_daemon_name=ORIGINATING"
   postconf -P "submission/inet/smtpd_recipient_restrictions=permit_sasl_authenticated,reject_unauth_destination"
+  postconf -P "submission/inet/tls_preempt_cipherlist=yes"
+
+  # only allow login from port 587
+  postconf -P "smtp/inet/smtpd_sasl_auth_enable=no"
 fi
 
 #############
@@ -128,6 +175,7 @@ cat >> /etc/opendkim/TrustedHosts <<EOF
 127.0.0.1
 localhost
 192.168.0.1/24
+172.16.0.0/12
 
 *.$maildomain
 EOF
