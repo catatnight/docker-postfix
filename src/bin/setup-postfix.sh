3659a1a3e6e11edcc558a982c4da13ee207029ed
Aubrey Hewes <AubreyHewes@users.noreply.github.com>
2020-02-11 14:14:55 +0100

    add support for removing received headers for a relay setup

--- src/bin/setup-postfix.sh
+++ src/bin/setup-postfix.sh
@@ -13,3 +13,8 @@ chmod +x /opt/postfix.sh
 
 postconf -e myhostname=$maildomain
 postconf -F '*/*/chroot = n'
+
+if [ "${STRIP_RECEIVED_HEADERS}" = "1" ]; then
+  echo "/^Received:.*/ IGNORE" >/etc/postfix/header_checks
+  echo "header_checks = pcre:/etc/postfix/header_checks" >>/etc/postfix/main.cf
+fi
