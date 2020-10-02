ea5be2760438cdcf83036f888001e1eeaadc5e15
Aubrey Hewes <AubreyHewes@users.noreply.github.com>
2020-10-02 20:32:13 +0200

    feat: added env option to set stricter rejects; reducing backscatter

--- src/bin/setup-postfix.sh
+++ src/bin/setup-postfix.sh
@@ -26,3 +26,27 @@ if [ "${STRIP_RECEIVED_HEADERS}" = "1" ]; then
   echo "/^Received:.*/ IGNORE" >/etc/postfix/header_checks
   postconf -e header_checks=pcre:/etc/postfix/header_checks
 fi
+
+if [ "${STRICT}" = "1" ]; then
+  STRICT_REJECT=1
+fi
+
+if [ "${STRICT_REJECT}" = "1" ]; then
+  # reduce backscatter
+  # taken from https://willem.com/blog/2019-09-10_fighting-backscatter-spam-at-server-level/
+  postconf -e smtpd_helo_required=yes
+  postconf -e invalid_hostname_reject_code=554
+  postconf -e multi_recipient_bounce_reject_code=554
+  postconf -e non_fqdn_reject_code=554
+  postconf -e relay_domains_reject_code=554
+  postconf -e unknown_address_reject_code=554
+  postconf -e unknown_client_reject_code=554
+  postconf -e unknown_hostname_reject_code=554
+  postconf -e unknown_local_recipient_reject_code=554
+  postconf -e unknown_relay_recipient_reject_code=554
+#  postconf -e unknown_sender_reject_code = 554 ## TODO unknown option in postfix?
+  postconf -e unknown_virtual_alias_reject_code=554
+  postconf -e unknown_virtual_mailbox_reject_code=554
+  postconf -e unverified_recipient_reject_code=554
+  postconf -e unverified_sender_reject_code=554
+fi
