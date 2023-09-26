636a0600c16f9bfdb2e088e7fbd14454f44e3705
Alex Noel <116229513+anoel2@users.noreply.github.com>
2023-09-26 13:39:14 -0600

    Update and rename export.bash to SMTPINFO.bash

---
+++ assets/SMTPINFO.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+#Put SMTP_USER and Password here. They will be exported as environmental variables, this will keep them protected. 
+export SMTP_USER='username'
+export SMTP_PASSWORD='password'
--- assets/export.bash
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export SMTP_USER='username'
-export SMTP_PASSWORD='password'
